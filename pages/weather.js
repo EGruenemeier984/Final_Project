b03e4f3c142458d7a749ea6978f6b3bdfcf5d919
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-12-17 12:06:07 -0700

    Created search field for Weather
    Started to implement a geocoding api to convert city to longitude and latitude.

--- pages/weather.js
+++ pages/weather.js
@@ -1,22 +1,67 @@
 import Navigation from '../components/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import fetch from 'isomorphic-unfetch';
 
 var lat = "32.3";
 var long = "32.23";
 
+
+
 const weather = props => (
+	
 <>
     <Navigation></Navigation>
     <h1 style={{textAlign: "center"}}>Weather</h1>
     <hr></hr>
-    <Form.Control type="text" placeholder="Normal text" />
-    <h3>Temperature</h3>
-    <h4>{props.weather.temperature}</h4>
+	<Container>
+		<Row>
+		<Form.Label>Place:</Form.Label>
+			<Col>
+				<Form.Group controlId="lat">
+					<Form.Control type="text" placeholder="City" />
+					<button data-input="#id-input" onClick={(e)=>this.saveKonfigElementHandler(e)}>Search</button>
+				</Form.Group>
+				
+			</Col>
+		</Row>
+	</Container>
+	<Jumbotron>
+		<Container>
+			<Row>
+				<Col>
+					<h3>Temperature</h3>
+    				<h5>{props.weather.temperature}&#8457;</h5>
+				</Col>
+
+				<Col>
+					<h3>Weather Summary</h3>
+					<h5>{props.weather.summary}</h5>
+				</Col>
+				<Col>
+					<h3>Stuff to Know</h3>
+					<ul>
+						<li>Humidity: {Math.round(props.weather.humidity * 100)}%</li>
+						<li>Wind Speed: {props.weather.windSpeed} MPH</li>
+						<li>Chance of Rain: {Math.round(props.weather.precipProbability * 100)}%</li>
+					</ul>
+				</Col>
+			</Row>
+			
+		</Container>
+		
+	</Jumbotron>
+   
     
 </>
+
 );
+
+
 
 weather.getInitialProps = async function() {
 	const res = await fetch(`https://api.darksky.net/forecast/4d821870011e1fdaf2e87a6c90a52d74/${lat},${long}`, {
@@ -25,12 +70,24 @@ weather.getInitialProps = async function() {
 			"key": "4d821870011e1fdaf2e87a6c90a52d74"
 		}
 	});
-	const data = await res.json();
+	const Weatherdata = await res.json();
 
-	console.log(`Logged Data: ${data}`);
+	console.log(`Logged Data: ${Weatherdata}`);
 
 	return{
-		weather: data.currently
+		weather: Weatherdata.currently
 	};
 };
+
+// weather.getInitialProps = async function() {
+// 	const res = await fetch(`https://api.geocod.io/v1.4/geocode?q=1109+N+Highland+St%2c+Arlington+VA&api_key=YOUR_API_KEY`);
+// 	const Locationdata = await res.json();
+
+// 	console.log(`Logged Data: ${Locationdata}`);
+
+// 	return{
+// 		lat: Locationdata.results.lat,
+// 		long: Locationdata.results.lng
+// 	};
+// };
 export default weather;
